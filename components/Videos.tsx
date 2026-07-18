bceb513ece50a850dca049d38d3a95224e9b6e3a
Nigel Otieno <nigel.otieno00@gmail.com>
2026-07-18 00:09:57 -0700

    build

--- components/Videos.tsx
+++ components/Videos.tsx
@@ -13,14 +13,14 @@ VIDEOS
 
 <div className="video">
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/oKf2HbqtqY0?si=QL3NKZWcopP2B43E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/oKf2HbqtqY0?si=QL3NKZWcopP2B43E" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
 
 </div>
 
 
 <div className="video">
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/oKf2HbqtqY0?si=4Iy3yvTjiN82SRmf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/oKf2HbqtqY0?si=4Iy3yvTjiN82SRmf" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
 
 </div>
 
